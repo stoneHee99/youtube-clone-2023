f0697276d04687b45998e458d4068a8f47594f18
Seokhee Park <101683784+stoneHee99@users.noreply.github.com>
2023-08-09 17:14:52 +0900

    update for mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,41 @@
-export const showTrending = (req, res) => res.render("home");
+const fakeUser = {
+  username: "Nicolas",
+  loggedIn: false,
+};
 
-export const see = (req, res) => res.render("watch");
+export const showTrending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", fakeUser, videos });
+};
 
-export const edit = (req, res) => res.render("edit");
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 
 export const search = (req, res) => res.send("Search");
 
