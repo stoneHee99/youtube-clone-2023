e2970c782c25312a953a9941d67803b56231e30d
Seokhee Park <101683784+stoneHee99@users.noreply.github.com>
2023-08-21 17:14:39 +0900

    update session

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,5 +1,6 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Tekken8Tube";
+  console.log(req.session.loggedIn);
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user;
   next();
